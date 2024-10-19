2a225ebea1bc2fa0f41901c7171728b4184f0fb4
princesinghrajput <psr8084@gmail.com>
2024-10-20 00:14:38 +0530

    added required deps

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  content: [
+    './src/**/*.{js,jsx,ts,tsx}', // Adjust this path if your file structure differs
+  ],
+  theme: {
+    extend: {
+      colors: {
+        'neon-blue': '#00eaff',
+        'neon-green': '#00ff9d',
+      },
+    },
+  },
+  plugins: [],
+}
